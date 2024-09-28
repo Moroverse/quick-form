7ee92cd866ff9f5ca4c6e02a9a694a2681e714af
Daniel Moro <daniel.moro@me.com>
2024-09-28 12:38:19 +0200

    Refactor fields

--- QuickFormDemo/Sources/PrescriptionFeature/UI/PrescriptionEditForm.swift
+++ QuickFormDemo/Sources/PrescriptionFeature/UI/PrescriptionEditForm.swift
@@ -5,73 +5,98 @@
 import QuickForm
 import SwiftUI
 
+extension UnitDose: Identifiable {}
+
+extension UnitDose: AllValues {
+    static var allCases: [UnitDose] = [
+        .application
+    ]
+}
+
+// func dummy(quickForm: PrescriptionEditModel) -> some View {
+//    FormValueUnitField(quickForm.take)
+// }
+
 struct PrescriptionEditForm: View {
     @Bindable private var quickForm: PrescriptionEditModel
+
+    private func substanceField() -> some View {
+        FormAsyncPickerField(
+            quickForm.medication.substance,
+            clearValueMode: .always,
+            pickerStyle: .navigation
+        ) { substance in
+            HStack {
+                Text("Substance:")
+                    .font(.headline)
+                Spacer()
+                Text(substance?.substance ?? "No substance selected")
+            }
+        } pickerContent: { info in
+            Text(info.substance)
+        }
+    }
+
+    private func strengthField() -> some View {
+        FormAsyncPickerField(
+            quickForm.medication.strength,
+            clearValueMode: .always,
+            pickerStyle: .navigation,
+            allowSearch: false
+        ) { strengthPart in
+            HStack {
+                Text("Strength:")
+                    .font(.headline)
+                Spacer()
+                Text(strengthPart?.strength.rawValue ?? "No strength selected")
+            }
+        } pickerContent: { strengthPart in
+            Text(strengthPart.strength.rawValue)
+        }
+    }
+
+    private func dosageFormField() -> some View {
+        FormAsyncPickerField(
+            quickForm.medication.dosageForm,
+            clearValueMode: .always,
+            allowSearch: false
+        ) { dosageFormPart in
+            HStack {
+                Text("Dosage Form:")
+                    .font(.headline)
+                Spacer()
+                Text(dosageFormPart?.form.rawValue ?? "No dosage form selected")
+            }
+        } pickerContent: { dosageFormPart in
+            Text(dosageFormPart.form.rawValue)
+        }
+    }
+
+    private func routeField() -> some View {
+        FormAsyncPickerField(
+            quickForm.medication.route,
+            clearValueMode: .always,
+            allowSearch: false
+        ) { routePart in
+            HStack {
+                Text("Route:")
+                    .font(.headline)
+                Spacer()
+                Text(routePart?.route.rawValue ?? "No route selected")
+            }
+        } pickerContent: { routePart in
+            Text(routePart.route.rawValue)
+        }
+    }
 
     var body: some View {
         Form {
             FormMultiPickerSection(quickForm.problems)
             Section("Medication") {
-                FormAsyncPickerField(
-                    quickForm.medication.substance,
-                    clearValueMode: .always,
-                    pickerStyle: .navigation
-                ) { substance in
-                    HStack {
-                        Text("Substance:")
-                            .font(.headline)
-                        Spacer()
-                        Text(substance?.substance ?? "No substance selected")
-                    }
-                } pickerContent: { info in
-                    Text(info.substance)
-                }
-
-                FormAsyncPickerField(
-                    quickForm.medication.strength,
-                    clearValueMode: .always,
-                    pickerStyle: .navigation,
-                    allowSearch: false
-                ) { strengthPart in
-                    HStack {
-                        Text("Strength:")
-                            .font(.headline)
-                        Spacer()
-                        Text(strengthPart?.strength.rawValue ?? "No strength selected")
-                    }
-                } pickerContent: { strengthPart in
-                    Text(strengthPart.strength.rawValue)
-                }
-
-                FormAsyncPickerField(
-                    quickForm.medication.dosageForm,
-                    clearValueMode: .always,
-                    allowSearch: false
-                ) { dosageFormPart in
-                    HStack {
-                        Text("Dosage Form:")
-                            .font(.headline)
-                        Spacer()
-                        Text(dosageFormPart?.form.rawValue ?? "No dosage form selected")
-                    }
-                } pickerContent: { dosageFormPart in
-                    Text(dosageFormPart.form.rawValue)
-                }
-
-                FormAsyncPickerField(
-                    quickForm.medication.route,
-                    clearValueMode: .always,
-                    allowSearch: false
-                ) { routePart in
-                    HStack {
-                        Text("Route:")
-                            .font(.headline)
-                        Spacer()
-                        Text(routePart?.route.rawValue ?? "No route selected")
-                    }
-                } pickerContent: { routePart in
-                    Text(routePart.route.rawValue)
-                }
+                substanceField()
+                strengthField()
+                dosageFormField()
+                routeField()
             }
         }
     }
