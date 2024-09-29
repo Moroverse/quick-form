05fcbfeab80bec1bf2cc6a2dada91f9b37494d3b
Daniel Moro <daniel.moro@me.com>
2024-09-29 06:24:18 +0200

    Introduce MessageDispatch

---
+++ Sources/QuickForm/MessageDispatch/Dispatcher.swift
@@ -0,0 +1,60 @@
+// Dispatcher.swift
+// Copyright (c) 2024 Moroverse
+// Created by Daniel Moro on 2024-09-29 04:24 GMT.
+
+import Foundation
+
+private struct ConcreteSubscription: Subscription {
+    private let _unsubscribe: () -> Void
+
+    init(unsubscribe: @escaping () -> Void) {
+        _unsubscribe = unsubscribe
+    }
+
+    public func unsubscribe() {
+        _unsubscribe()
+    }
+}
+
+public class Dispatcher {
+    private protocol AnyHandler {}
+    private struct TypedHandler<T>: AnyHandler {
+        let id: UUID
+        let handler: (T) -> Void
+    }
+
+    private var registrations: [AnyHandler]
+
+    public init() {
+        registrations = []
+    }
+
+    public func publish<T>(_ event: T) {
+        for typedHandler in registrations {
+            if let typedHandler = typedHandler as? TypedHandler<T> {
+                typedHandler.handler(event)
+            }
+        }
+    }
+
+    public func subscribe<T>(handler: @escaping @Sendable (T) -> Void) -> Subscription {
+        let key = UUID()
+
+        let typedHandler = TypedHandler(id: key, handler: handler)
+
+        registrations.append(typedHandler)
+
+        return ConcreteSubscription { [weak self] in
+            if let index = self?.registrations.firstIndex(where: {
+                if let typedHandler = $0 as? TypedHandler<T>,
+                   typedHandler.id == key {
+                    true
+                } else {
+                    false
+                }
+            }) {
+                self?.registrations.remove(at: index)
+            }
+        }
+    }
+}
---
+++ Sources/QuickForm/MessageDispatch/Subscription.swift
@@ -0,0 +1,8 @@
+// Subscription.swift
+// Copyright (c) 2024 Moroverse
+// Created by Daniel Moro on 2024-09-29 04:24 GMT.
+
+public protocol Subscription {
+    /// Unsubscribes from the event, stopping further notifications.
+    func unsubscribe()
+}
