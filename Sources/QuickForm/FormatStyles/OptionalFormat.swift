46731615ee3921d0dbcd00f2c84e7048309db006
Daniel Moro <daniel.moro@me.com>
2025-03-04 09:27:47 +0100

    Extract PlainStringFormat

--- Sources/QuickForm/FormatStyles/OptionalFormat.swift
+++ Sources/QuickForm/FormatStyles/OptionalFormat.swift
@@ -93,21 +93,3 @@ public struct OptionalFormatStrategy<Value, S>: ParseStrategy
         self.strategy = strategy
     }
 }
-
-public struct PlainStringStrategy: ParseStrategy {
-    public func parse(_ value: String) throws -> String {
-        value
-    }
-}
-
-public struct PlainStringFormat: ParseableFormatStyle {
-    public var parseStrategy: PlainStringStrategy {
-        PlainStringStrategy()
-    }
-
-    public func format(_ value: String) -> String {
-        value
-    }
-
-    public init() {}
-}
---
+++ Sources/QuickForm/FormatStyles/PlainStringFormat.swift
@@ -0,0 +1,23 @@
+// PlainStringStrategy.swift
+// Copyright (c) 2025 Moroverse
+// Created by Daniel Moro on 2025-03-04 07:30 GMT.
+
+import Foundation
+
+public struct PlainStringFormat: ParseableFormatStyle {
+    public var parseStrategy: PlainStringStrategy {
+        PlainStringStrategy()
+    }
+
+    public func format(_ value: String) -> String {
+        value
+    }
+
+    public init() {}
+}
+
+public struct PlainStringStrategy: ParseStrategy {
+    public func parse(_ value: String) throws -> String {
+        value
+    }
+}
